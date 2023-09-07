9e7a5bb8c3747dc9b9999338794962df8194358f
JohannesSchambortzki <144218493+JohannesSchambortzki@users.noreply.github.com>
2023-09-07 11:34:02 +0200

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,3 +1,4 @@
+ösldkföjklsdaf
 docker run -it -v /home/jos/Cordova/shared-volume:/root/shared-volume beevelop/cordova /bin/bash -c "cd /root/shared-volume/MyApp/ && yes n | cordova run android && send "n\r" && touch hello"
 
 
